63e43d0d25e86c457ec562f931db81a429075a04
WaterAndWood <iwangzhen1992@163.com>
2020-10-29 00:05:14 +0800

    LeetCode 692:前k个高频单词
    Signed-off-by: WaterAndWood <iwangzhen1992@163.com>

---
+++ src/SortAlgorithm/TopKWord.java
@@ -0,0 +1,83 @@
+package SortAlgorithm;
+
+import java.util.*;
+
+/**
+ *
+ * LeetCode 692: 前K个高频单词
+ *
+ * @author Richa
+ * @date 2020/10/28 22:03
+ */
+public class TopKWord {
+    /**
+     * 统计出现次数，排序后取K
+     * 时间复杂度O(NlogN)，N是words的长度
+     */
+    public List<String> topKWord(String[] words, int k) {
+        Map<String, Integer> count = new HashMap<>();
+        for (String word : words) {
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        }
+        
+        List<String> candidates = new ArrayList<>(count.keySet());
+        // 从大到小排序
+        Collections.sort(candidates, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (count.get(o1).equals(count.get(o2))) {
+                    return o1.compareTo(o2);
+                } else {
+                    return count.get(o2) - count.get(o1);
+                }
+            }
+        });
+        return candidates.subList(0, k);
+    }
+    
+    /**
+     * 最好使用堆排序，使用空间为k的小根堆，频率最小的放在堆顶部
+     * 堆中弹出最多 k 次，并反转结果，就可以得到前 k 个高频单词。
+     * 时间复杂度O(Nlogk)，N是words的长度
+     * 用O(N) 的时间计算每个单词的频率，然后将 N 个单词添加到堆中，添加每个单词的时间为 O(logk)
+     */
+    public List<String> topKWordHeap(String[] words, int k) {
+        Map<String, Integer> count = new HashMap<>();
+        for (String word : words) {
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        }
+
+        // 次数相等的情况并且小根堆
+        PriorityQueue<String> minHeap = new PriorityQueue<String>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (count.get(o1).equals(count.get(o2))) {
+                    return o2.compareTo(o1);
+                } else {
+                    return count.get(o1) - count.get(o2);
+                }
+
+            }
+        });
+
+        for (String word : count.keySet()) {
+            minHeap.offer(word);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        List<String> result = new ArrayList<>();
+        while (!minHeap.isEmpty()) {
+            result.add(minHeap.poll());
+        }
+        Collections.reverse(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"a", "a", "b", "a", "b", "c", "c", "d"};
+        int k = 2;
+        TopKWord topKWord = new TopKWord();
+        System.out.println(topKWord.topKWordHeap(words, k));
+    }
+}
