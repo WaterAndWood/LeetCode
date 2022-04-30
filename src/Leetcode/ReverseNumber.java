ee01f66c328986a7e5b5a42239fb9ad9117246c2
WaterAndWood <iwangzhen1992@163.com>
2022-04-30 08:59:50 +0800

    整数反转

---
+++ src/Leetcode/ReverseNumber.java
@@ -0,0 +1,30 @@
+package Leetcode;
+
+/**
+ *
+ * LeetCode7: 整数反转
+ * 考虑int的边界值-2^31， 2^31-1
+ * 
+ * @author Richa
+ * @date 2022/4/29 22:00
+ */
+public class ReverseNumber {
+    public int reverseNum(int x) {
+        int ans = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            /**
+             * 判断是否越界
+             */
+            if ((ans > Integer.MAX_VALUE / 10) || (ans == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if ((ans < Integer.MIN_VALUE / 10) || (ans == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            ans = ans * 10 + pop;
+            x /= 10;
+        }
+        return ans;
+    }
+}
